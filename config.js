3e3cd1a9b5b2b883cc8fefdc117dde5a53422819
paulmnisi0123-ux <paulmnisi0123@gmail.com>
2026-08-19 20:25:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 //  This uses the v3 API key.
 // ────────────────────────────────────────────────────────────
 const CONFIG = {
-  TMDB_API_KEY: "9e677dd0efbd2e9eb9c966353b5bf2f0",
+  TMDB_API_KEY: "78d8ca68310e1f4bbd5b4808a5e21ac0",
   TMDB_BASE: "https://api.themoviedb.org/3",
   IMG_BASE: "https://image.tmdb.org/t/p/w500",
   IMG_BASE_LARGE: "https://image.tmdb.org/t/p/original",
